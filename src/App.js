dfa122142cc34292bd340fbf72d400e73d549d67
Sebastian Duarte <Subsono33@gmail.com>
2020-12-09 00:37:49 -0500

    added react-twitter-embed package. started working on widget component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Sidebar from './Sidebar';
-import Feed from './Feed'
-import Widgets from './Widgets'
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     <div className="app">
-      
-
-      {/* sidebar */}
       <Sidebar />
 
-      {/* feed */}
       <Feed />
 
-      {/* widget */}
       <Widgets />
     </div>
   );
 }
 
 export default App;
-
-
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Widgets.css'
+import SearchIcon from "@material-ui/Search"
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widget__searchIcon" />
+        <input placeholder="Search Twitter" type="text"></input>
+      </div>
     </div>
   )
 }
