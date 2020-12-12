3a5f1e1251bcfdbab44a8aa1b3003c4333546cab
Sebastian Duarte <Subsono33@gmail.com>
2020-12-12 00:06:50 -0500

    added react flip move

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,7 @@ import TweetBox from "./TweetBox";
 import "./Feed.css";
 import Post from "./Post";
 import db from "./firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -20,16 +21,19 @@ function Feed() {
 
       <TweetBox />
 
-      {posts.map((post) => (
-        <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-        />
-      ))}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
